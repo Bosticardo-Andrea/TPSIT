afc956f2a358cb7bb6c2891cfb179c8967906d92
Bosticardo-Andrea <90908021+Bosticardo-Andrea@users.noreply.github.com>
2021-10-01 10:36:44 +0200

    Correzione

--- Es_002_VotiPerMateria/main.c
+++ Es_002_VotiPerMateria/main.c
@@ -13,6 +13,7 @@ typedef struct {
     int ita[MAX_STD/5];
     int mate[MAX_STD/5];
     int info[MAX_STD/5];
+    int mi,minf,mm;
 } Studente;
 
 void leggi(Studente s[], int n, int *it, int *inf, int *m) {
@@ -24,29 +25,29 @@ void leggi(Studente s[], int n, int *it, int *inf, int *m) {
         printf("Inserisci quanti voti per ita: ");
         scanf("%d",&num);
         for(int k = 0; k < num; k++) {
-        do {
-            printf("Inserisci il voto: ");
-            scanf("%d",&s[i].ita[k]);
-        } while(s[i].ita[k] < 0);
-    }
-    *it = num;
-    printf("Inserisci quanti voti per mate: ");
+            do {
+                printf("Inserisci il voto: ");
+                scanf("%d",&s[i].ita[k]);
+            } while(s[i].ita[k] < 0);
+        }
+        *it = num;
+        printf("Inserisci quanti voti per mate: ");
         scanf("%d",&num);
         for(int k = 0; k < num; k++) {
-        do {
-            printf("Inserisci il voto: ");
-            scanf("%d",&s[i].ita[k]);
-        } while(s[i].ita[k] < 0);
-    }
-    *m = num;
-    printf("Inserisci quanti voti per info: ");
+            do {
+                printf("Inserisci il voto: ");
+                scanf("%d",&s[i].ita[k]);
+            } while(s[i].ita[k] < 0);
+        }
+        *m = num;
+        printf("Inserisci quanti voti per info: ");
         scanf("%d",&num);
         for(int k = 0; k < num; k++) {
-        do {
-            printf("Inserisci il voto: ");
-            scanf("%d",&s[i].ita[k]);
-        } while(s[i].ita[k] < 0);
-    }
+            do {
+                printf("Inserisci il voto: ");
+                scanf("%d",&s[i].ita[k]);
+            } while(s[i].ita[k] < 0);
+        }
     }
     *inf = num;
 }
@@ -58,28 +59,54 @@ int dimensione(int max) {
     } while(n < 0 && n > max);
     return n;
 }
-void media(int *mi, int *minf, int *mm,Studente s[], int ni, int inf, int m,int n){
+void media(Studente s[], int ni, int inf, int mat,int n) {
     int sm,si,sinf;
     sm = 0;
     si = 0;
     sinf = 0;
+    for(int k = 0; k < 0; k++) {
+        s[k].mi = 1;
+        s[k].minf = 1;
+        s[k].mm = 1;
+        for(int i = 0; i < ni; i ++) {
+            si = si + s[k].ita;
+        }
+        for(int j = 0; j < inf; j ++) {
+            sinf = sinf + s[k].info;
+        }
+        for(int f = 0; f < mat; f ++) {
+            sm = sm + s[k].mate;
+        }
+        s[k].mi = sm/mat;
+        s[k].minf = sm/inf;
+        s[k].mm = sm/ni;
+    }
 }
-
+void stampa(Studente s[], int n) {
+    for(int i = 0; i < n; i++) {
+        if(s[i].mi > s[i].minf) {
+            if(s[i].mi > s[i].mm){
+            printf("Lo studente %s ha la media alta in Italiano",s[i].nome);
+            }else if(s[i].minf > s[i].mm){
+                printf("Lo studente %s ha la media alta in Informatica",s[i].nome);
+            }
+            }else{
+                printf("Lo studente %s ha la media alta in mate",s[i].nome);
+            }
+    }
+}
 int main() {
+    Studente s[DIM/2];
     int n = 0;
     int ni,ninf,nm;
-    int mi,minf,mm;
     ni = 0;
     ninf = 0;
     nm = 0;
-    mi = 1;
-    minf = 1;
-    mm = 1;
-    Studente s[DIM/2];
+
     n = dimensione(MAX_STD);
     leggi(s,n,&ni,&ninf,&nm);
-    media(&mi,&minf,&mm,s,ni,ninf,nm,n);
-    //stampa(s,n);
+    media(s,ni,ninf,nm,n);
+    stampa(s,n);
 
     printf("\n");
     system("PAUSE");
